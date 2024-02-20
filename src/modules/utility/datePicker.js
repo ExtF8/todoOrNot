8768e7a1b40f07b73c03c799dea96c847eaf243e
extf8 <extfield8@gmail.com>
2024-02-20 19:14:42 +0200

    Fixed datePickerHandler bug

--- src/modules/utility/datePicker.js
+++ src/modules/utility/datePicker.js
@@ -2,15 +2,17 @@ import flatpickr from 'flatpickr';
 import { format } from 'date-fns';
 import 'flatpickr/dist/flatpickr.min.css';
 
-document.addEventListener('DOMContentLoaded', function () {
+
+export function datePickerHandler() {
     // Initialize Flatpickr on the dueDate input field
     const dueDateInput = document.getElementById('dueDate');
 
     flatpickr(dueDateInput, {
-        dateFormat: 'Y-m-d',
-        minDate: 'today',
+        static: true,
         altInput: true,
-        altFormat: 'F j, Y',
+        altFormat: "d/m/Y",
+        dateFormat: "Y-m-d",
+        minDate: 'today',
     });
 
     // Add an event listener to the input field to format the date using date-fns when it changes
@@ -19,4 +21,6 @@ document.addEventListener('DOMContentLoaded', function () {
         const formattedDate = format(selectedDate, 'MMMM dd, yyyy');
         console.log(formattedDate);
     });
-});
+
+}
+

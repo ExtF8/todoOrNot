468cb36a24a4e9691a4f46848fed2bc4332f1d5f
extf8 <extfield8@gmail.com>
2024-02-27 18:16:17 +0200

    Refactored methods

--- src/modules/entities/project.js
+++ src/modules/entities/project.js
@@ -4,7 +4,6 @@ export class Project {
         this.name = name;
         this.todos = [];
     }
-
 }
 
 export class ProjectManager {
@@ -13,13 +12,12 @@ export class ProjectManager {
     }
 
     addTodoToProject(todo) {
-        let project = this.getProjectsByName(todo.project);
-        let id = Date.now();
-
+        let project = this.getProjectByName(todo.project);
 
         if (project) {
             project.todos.push(todo);
         } else {
+            const id = Date.now();
             project = new Project(id, todo.project);
             this.projects.push(project);
             project.todos.push(todo);
@@ -27,15 +25,19 @@ export class ProjectManager {
     }
 
     removeTodoFromProject(projectName, todoId) {
-        const project = this.getProjectsByName(projectName);
+        const project = this.getProjectByName(projectName);
         if (project) {
             project.todos = project.todos.filter((todo) => todo.id !== todoId);
         } else {
-            console.log('No todos found');
+            console.log('No project found with name:', projectName);
         }
     }
 
-    getProjectsByName(name) {
+    getProjectByName(name) {
         return this.projects.find((project) => project.name === name);
+    }
+
+    getProjectById(id) {
+        return this.projects.find((project) => project.id === id);
     }
 }

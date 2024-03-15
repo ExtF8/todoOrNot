95da065644de4e3ad1e34995e31ff746526b55a7
extf8 <extfield8@gmail.com>
2024-03-15 14:05:25 +0200

    Added try catch

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ import './styles/dark.css';
 import darkModeToggler from './modules/utility/darkMode.js';
 import mobileNavigationToggling from './modules/utility/mobileNavigation.js';
 import { dialogHandler } from './modules/utility/dialogHandler.js';
+import { handleDialogError } from './modules/utility/todoRenderer.js';
 
 // Import images
 import logo from './assets/img/logo/todoOrNotLogo.png';
@@ -25,17 +26,17 @@ import todayPageLoader from './modules/pageLoaders/todayPageLoader.js';
 import weekPageLoader from './modules/pageLoaders/weekPageLoader.js';
 import projectsPageLoader from './modules/pageLoaders/projectsPageLoader.js';
 
-// Activate dark mode toggle functionality
-darkModeToggler();
-
-// Set up mobile navigation toggle functionality
-mobileNavigationToggling();
-
 // Global variables setup
 // Navigation button IDs
 const navigationButtons = ['home', 'today', 'week', 'projects'];
 const newTodoButton = document.getElementById('newTodoButton');
 const content = document.getElementById('content');
+
+// Activate dark mode toggle functionality
+darkModeToggler();
+
+// Set up mobile navigation toggle functionality
+mobileNavigationToggling();
 
 // Load the initial home page content
 homePageLoader(content);
@@ -46,36 +47,47 @@ homePageLoader(content);
  * (home, todos for today, todos for this week, projects) and
  * ensures the appropriate content is loaded and displayed.
  */
-navigationButtons.forEach((navigationButtons) => {
-    const button = document.getElementById(navigationButtons);
+try {
+    navigationButtons.forEach((navigationButtons) => {
+        const button = document.getElementById(navigationButtons);
 
-    button.addEventListener('click', () => {
-        switch (navigationButtons) {
-            case 'home':
-                homePageLoader(content);
-                break;
-            case 'today':
-                todayPageLoader(content);
-                break;
-            case 'week':
-                weekPageLoader(content);
-                break;
-            case 'projects':
-                projectsPageLoader(content);
-                break;
-            default:
-                homePageLoader(content); // Default page if page is not specified
-        }
-        updateNavigationActiveState(navigationButtons);
+        button.addEventListener('click', () => {
+            switch (navigationButtons) {
+                case 'home':
+                    homePageLoader(content);
+                    break;
+                case 'today':
+                    todayPageLoader(content);
+                    break;
+                case 'week':
+                    weekPageLoader(content);
+                    break;
+                case 'projects':
+                    projectsPageLoader(content);
+                    break;
+                default:
+                    homePageLoader(content); // Default page if page is not specified
+            }
+            updateNavigationActiveState(navigationButtons);
+        });
     });
-});
+} catch (error) {
+    console.error(
+        'Error setting up event listeners for tab navigation: ',
+        error
+    );
+}
 
 /**
  * Handles the click event for the "New Todo" button and opens a dialog.
  */
-newTodoButton.addEventListener('click', async () => {
-    await dialogHandler(newTodoButton, 'newTodoButton');
-});
+try {
+    newTodoButton.addEventListener('click', async () => {
+        await dialogHandler(newTodoButton, 'newTodoButton');
+    });
+} catch (error) {
+    handleDialogError(error);
+}
 
 /**
  * Updates the active state of navigation buttons and removes active state in mobile navigation.
@@ -88,19 +100,23 @@ export function updateNavigationActiveState(activeButtonId) {
     const navbarMenu = document.querySelector('#cs-navigation');
     const hamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
 
-    navigationButtons.forEach((buttonId) => {
-        const buttonElement = document.getElementById(buttonId);
-        if (buttonElement) {
-            if (buttonId === activeButtonId) {
-                buttonElement.classList.add('cs-active');
+    try {
+        navigationButtons.forEach((buttonId) => {
+            const buttonElement = document.getElementById(buttonId);
+            if (buttonElement) {
+                if (buttonId === activeButtonId) {
+                    buttonElement.classList.add('cs-active');
 
-                // Close mobile navigation menu when a navigation button is activated
-                hamburgerMenu.classList.remove('cs-active');
-                navbarMenu.classList.remove('cs-active');
-                body.classList.remove('cs-open');
-            } else {
-                buttonElement.classList.remove('cs-active');
+                    // Close mobile navigation menu when a navigation button is activated
+                    hamburgerMenu.classList.remove('cs-active');
+                    navbarMenu.classList.remove('cs-active');
+                    body.classList.remove('cs-open');
+                } else {
+                    buttonElement.classList.remove('cs-active');
+                }
             }
-        }
-    });
+        });
+    } catch (error) {
+        console.error('Error updating navigation state: ', error);
+    }
 }

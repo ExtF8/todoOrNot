e50ee44f49de8d4823f9057d4c3836745890bc00
extf8 <extfield8@gmail.com>
2024-03-15 14:05:56 +0200

    Refactored homePageLoader function

--- src/modules/pageLoaders/homePageLoader.js
+++ src/modules/pageLoaders/homePageLoader.js
@@ -1,6 +1,6 @@
 import { clearPage } from '../utility/elementRender.js';
 
-import { renderContainer } from '../utility/todoRenderer.js';
+import { handleStorageError, renderContainer } from '../utility/todoRenderer.js';
 
 import { ProjectManager } from '../entities/project.js';
 
@@ -21,31 +21,31 @@ const PROJECTS_STORAGE_KEY = 'projects';
  * @param {HTMLElement} content - The parent element where the home page will be rendered.
  */
 export default function homePageLoader(content) {
-    document.addEventListener('DOMContentLoaded', async function () {
-        clearPage(content);
-        let existingData;
+    clearPage(content);
+    let existingData;
 
-        try {
-            // Try to retrieve existing data from local storage
-            existingData = getDataFromLocalStorage(PROJECTS_STORAGE_KEY);
-            // Check if existingData is null or undefined
-            if (!existingData) {
-                // If no existingData exists in local storage, save sample data
-                saveDataToLocalStorage(
-                    PROJECTS_STORAGE_KEY,
-                    sampleData.projects
-                );
-            }
-        } catch (error) {
-            handleStorageError(error);
-            // Save sample data to local storage as as fallback
-            saveDataToLocalStorage(PROJECTS_STORAGE_KEY, sampleData.projects);
+    try {
+        // Try to retrieve existing data from local storage
+        existingData = getDataFromLocalStorage(PROJECTS_STORAGE_KEY);
+        // Check if existingData is null or undefined
+        if (!existingData) {
+            // If no existingData exists in local storage, save sample data
+            saveDataToLocalStorage(
+                PROJECTS_STORAGE_KEY,
+                sampleData.projects
+            );
         }
+    } catch (error) {
+        handleStorageError(error);
+        // Save sample data to local storage as as fallback
+        saveDataToLocalStorage(PROJECTS_STORAGE_KEY, sampleData.projects);
+    }
 
-        const section = document.querySelector('#content');
-        const todoContainer = renderContainer(existingData);
-        section.appendChild(todoContainer);
-    });
+    const section = document.querySelector('#content');
+    const todoContainer = renderContainer(existingData);
+    section.appendChild(todoContainer);
+
+
 }
 
 export { projectManager };

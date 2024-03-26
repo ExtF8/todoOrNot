6ea59c37fd32ab32806c8340bf7a54bac806f43c
extf8 <extfield8@gmail.com>
2024-03-26 16:31:20 +0200

    Refactored code and comments

--- src/modules/utility/mobileNavigation.js
+++ src/modules/utility/mobileNavigation.js
@@ -7,28 +7,21 @@ const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
  * Sets up event listeners for mobile navigation toggling
  */
 export default function mobileNavigationToggling() {
+    // Event listener for toggling the navigation menu when hamburger menu is clicked
     CShamburgerMenu.addEventListener('click', function () {
         CShamburgerMenu.classList.toggle('cs-active');
         CSnavbarMenu.classList.toggle('cs-active');
         CSbody.classList.toggle('cs-open');
-        // run the function to check the aria-expanded value
+        // Toggle aria-expanded attribute
         ariaExpanded();
     });
 
-    function classListToggle(params) {
-        CShamburgerMenu.classList.toggle('cs-active');
-        CSnavbarMenu.classList.toggle('cs-active');
-        CSbody.classList.toggle('cs-open');
-    }
-
-    /**
-     * Checks and toggles the 'aria-expanded' attribute on the navigation list
-     */
+    // Function to toggle the 'aria-expanded' attribute on the navigation list
     function ariaExpanded() {
         const csUL = document.querySelector('#cs-expanded');
         const csExpanded = csUL.getAttribute('aria-expanded');
 
-        // Toggle the 'aria-expanded' attribute based on its current state.
+        // Toggle the 'aria-expanded' attribute based on its current state
         if (csExpanded === 'false') {
             csUL.setAttribute('aria-expanded', 'true');
         } else {
@@ -41,6 +34,7 @@ export default function mobileNavigationToggling() {
         document.querySelectorAll('#cs-navigation .cs-dropdown')
     );
     for (const item of dropDowns) {
+        // Event listener for toggling dropdown visibility
         const onClick = () => {
             item.classList.toggle('cs-active');
         };
